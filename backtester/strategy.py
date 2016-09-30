56f5ef3465e0b877a8e0b98b5dbc760af4b5fee1
Abdullah Yousufi <ayousufi@rshieldtech.com>
2016-09-29 19:02:55 -0700

    Create class for simple trends algo

--- backtester/strategy.py
+++ backtester/strategy.py
@@ -18,6 +18,38 @@ class Strategy(object):
     def calculate_signals(self):
         raise NotImplementedError("calculate_signals() is not implemented")
 
+class SimpleTrendsStrategy(Strategy):
+    def __init__(self, bars, events):
+        self.bars = bars
+        self.symbol_list = self.bars.symbol_list
+        self.events = events
+
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+        bought = {}
+        for s in self.symbol_list:
+            bought[s] = False
+        return bought
+
+    def calculate_signals(self, event):
+        if event.type == 'MARKET':
+            for s in self.symbol_list:
+                bars = self.bars.get_latest_bars(s, N=1)
+                if bars is not None and bars != []:
+                    if self.bought[s] == False:
+                        # (Symbol, Datetime, Type = LONG, SHORT, EXIT)
+                        signal = SignalEvent(bars[0][0], bars[0][1], 'LONG')
+                        self.events.put(signal)
+                        time.sleep(0.01)
+                        self.bought[s] = True
+                    else:
+                        signal = SignalEvent(bars[0][0], bars[0][1], 'EXIT')
+                        self.events.put(signal)
+                        time.sleep(0.01)
+                        self.bought[s] = False
+
+
 class BuyAndHoldStrategy(Strategy):
     "Example strategy that goes LONG on all the symbols"
     def __init__(self, bars, events):
